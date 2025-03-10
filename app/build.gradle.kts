4e18d5d24ba6d364f890fb63c5ad61838c785c86
Gh05t-1337 <95411780+Gh05t-1337@users.noreply.github.com>
2025-03-10 01:11:56 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.autismprime.fall"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
